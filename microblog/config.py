70a431645f627891affeb3ab152338a30bcd1444
Areid0093 <54606271+Areid0093@users.noreply.github.com>
2020-03-17 13:37:07 -0500

    emails+translations

--- microblog/config.py
+++ microblog/config.py
@@ -11,5 +11,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['alecreid0093@protonmail.com']
+    ADMINS = ['example@example.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
